fe1be3c9aa5184b8e8cf2646006295c5c43bc930
Kasey <gforcetd@gmail.com>
2019-08-24 16:53:20 -0600

    v0.1

---
+++ light_control.h
@@ -0,0 +1,115 @@
+// Define light
+#define NUM_MATRICES 3
+#define NUM_LEDS 64 * NUM_MATRICES
+#define DATA_PIN 18
+#define CLOCK_PIN 5
+
+// Setup light params
+CRGB matrix[NUM_LEDS];
+int individualMatrixWidth = 8;
+int individualMatrixHeight = 8;
+const int moduleHeight = 1;
+const int moduleWidth = 3;
+int matrixMap[moduleHeight][moduleWidth] = {{0,1,2}};
+int orientation[moduleHeight * moduleWidth] = {3,3,3};
+
+// Setup other params
+int bootCycle = individualMatrixWidth * moduleWidth -1;
+int cancelCycle = individualMatrixWidth * moduleWidth -1;
+
+void fadeAll(uint8_t scale = 250) {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    matrix[i].nscale8(scale);
+  }
+}
+
+uint16_t XY( uint16_t x, uint16_t y) {
+  uint16_t i;
+  uint8_t matrixNum = matrixMap[(moduleHeight - 1) - (y / individualMatrixHeight)][x / individualMatrixWidth]; //Calculates which matrix we are on using the matrix map
+  uint8_t relativeX; //x position relative to the origin of the current matrix
+  uint8_t relativeY; //y position relative to the origin of the current matrix
+  switch (orientation[matrixNum])//Transposes our values based on orientation
+  {
+    case 0:
+      relativeX = x % individualMatrixWidth;
+      relativeY = y % individualMatrixHeight;
+      break;
+    case 1:
+      relativeX = 7 - (y % individualMatrixWidth);
+      relativeY = x % individualMatrixHeight;
+      break;
+    case 2:
+      relativeX = 7 - (x % individualMatrixWidth);
+      relativeY = y % individualMatrixHeight;
+      break;
+    case 3:
+      relativeX = y % individualMatrixWidth;
+      relativeY = x % individualMatrixHeight;
+      break;
+  }
+  i = (relativeY * individualMatrixWidth) + relativeX; //Calculates where we are on a matrix
+  i += matrixNum * individualMatrixWidth * individualMatrixHeight; //Adds on offset based on which matrix we're on in the chain.
+  return i;
+}
+
+
+
+void lightOff() {
+  for (int y = 0; y < (individualMatrixHeight *  moduleHeight); y++) {
+    for (int x = individualMatrixWidth * moduleWidth -1; x >= 0; x--) {
+      matrix[XY(x, y)].setRGB(0,0,0);
+    }
+  }
+
+  FastLED.show();
+}
+
+void bootLight(bool booting = true) {
+  if(booting) {
+    for (int y = 0; y < (individualMatrixHeight *  moduleHeight); y++) {
+      matrix[XY(bootCycle, y)].setRGB(0,0,255);
+    }
+
+    FastLED.show();
+    delay(10);
+    fadeAll(120);
+
+    if(bootCycle == 0) {
+      bootCycle = individualMatrixWidth * moduleWidth - 1;
+    } else {
+      bootCycle--;
+    }
+  } else {
+    lightOff();
+  }
+}
+
+void errorLight() {
+  while(true) {
+    for (int x = individualMatrixWidth * moduleWidth -1; x >= 0; x--) {
+      for (int y = 0; y < (individualMatrixHeight *  moduleHeight); y++) {
+        matrix[XY(x, y)].setRGB(255,255,255);
+      }
+
+      FastLED.show();
+      delay(50);
+      fadeAll(120);
+    }
+  }
+}
+
+void updateLight(int r, int g, int b, unsigned int delayTime = 0) {
+  for (int y = 0; y < (individualMatrixHeight *  moduleHeight); y++) {
+      matrix[XY(cancelCycle, y)].setRGB(r,g,b);
+  }
+  
+  FastLED.show();
+  delay(delayTime);
+  fadeAll(120);
+
+  if(cancelCycle == 0) {
+    cancelCycle = individualMatrixWidth * moduleWidth - 1;
+  } else {
+    cancelCycle--;
+  }
+}
--- reflow-o-meter.ino
+++ reflow-o-meter.ino
@@ -1,160 +1,197 @@
-/*
-  Reading distance from the laser based VL53L1X
-  By: Nathan Seidle
-  SparkFun Electronics
-  Date: April 4th, 2018
-  License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
-
-  SparkFun labored with love to create this code. Feel like supporting open source hardware? 
-  Buy a board from SparkFun! https://www.sparkfun.com/products/14667
-
-  This example demonstrates how to read and average distance, the measurement status, and the signal rate.
-
-  Are you getting weird readings? Be sure the vacuum tape has been removed from the sensor.
-*/
-
 #include <Wire.h>
+#include <FastLED.h>
 #include "SparkFun_VL53L1X.h"
+#include "light_control.h"
 
-//Optional interrupt and shutdown pins.
-#define SHUTDOWN_PIN 2
-#define INTERRUPT_PIN 3
-
+// Setup detectors
 SFEVL53L1X distanceSensor;
-//Uncomment the following line to use the optional shutdown and interrupt pins.
-//SFEVL53L1X distanceSensor(Wire, SHUTDOWN_PIN, INTERRUPT_PIN);
-
-//Store distance readings to get rolling average
-#define HISTORY_SIZE 5
-int history[HISTORY_SIZE];
-byte historySpot;
-bool board = true;
-long cal = 0;
-void setup(void)
-
 #define NUMBER_OF_SENSORS 3
-{
+#define CALIBRATION_SAMPLE 500
+//Store distance readings to get rolling average
+#define HISTORY_SIZE 8
+int history[NUMBER_OF_SENSORS][HISTORY_SIZE];
+byte historySpot[NUMBER_OF_SENSORS];
+bool board = true;
+long calibration[NUMBER_OF_SENSORS];
+int clearReading = 3;
+#define DIFF_FACTOR 20
+
+#define INTERRUPT_PIN 21
+volatile bool cancelled;
+
+void IRAM_ATTR cancelLight() {
+  cancelled = true;
+}
+
+void setup(void){
   Wire.begin();
 
   Serial.begin(9600);
-  Serial.println("VL53L1X Qwiic Test");
 
+  // Setup light
+  LEDS.addLeds<APA102, DATA_PIN, CLOCK_PIN, BGR, DATA_RATE_MHZ(1)>(matrix, NUM_LEDS);
+  LEDS.setBrightness(10);
+  lightOff();
+
+  // Boot
+  Serial.println("Welcome to the Reflow-o-Meter");
+  Serial.println("Booting...");
+
+  bool errors = false;
+
+  bootLight();
+ 
+  // Setup detectors
+  Serial.println("\tSetting up detectors...");
+  
   for (byte x = 0 ; x < NUMBER_OF_SENSORS ; x++) {
     enableMuxPort(x); //Tell mux to connect to port X
 
-    if (distanceSensor.begin() == true)
-      Serial.println("Sensor online!");
+    if (distanceSensor.init() == false) {
+      if(distanceSensor.getSensorID() != 0) {
+        Serial.print("\tDetector ");
+        Serial.print(x + 1);
+        Serial.println(" online");
 
-    distanceSensor.setDistanceModeShort();
-    
+        distanceSensor.setDistanceModeShort();
+
+        // Calibrate
+        Serial.println("\tCalibrating Detector");
+
+        calibration[x] = 0;
+        int calibrationHistory[CALIBRATION_SAMPLE];
+        
+        for(int i = 0; i < CALIBRATION_SAMPLE; i++) {
+          bootLight();
+          distanceSensor.startRanging(); //Write configuration block of 135 bytes to setup a measurement
+          calibrationHistory[i] = distanceSensor.getDistance(); //Get the result of the measurement from the sensor
+          distanceSensor.stopRanging();
+        }
+
+        // Calculate calibration
+        for (int i = 0 ; i < CALIBRATION_SAMPLE; i++) {
+          calibration[x] += calibrationHistory[i];
+        }
+
+        calibration[x] /= CALIBRATION_SAMPLE;
+
+        Serial.print("\tCalibration ");
+        Serial.print(calibration[x]);
+        Serial.println("mm");
+
+        // Set up history
+        for (int i = 0 ; i < HISTORY_SIZE ; i++) {
+          history[x][i] = calibration[x];
+        }
+        
+        Serial.println("\tFinished Calibrating Detector");
+      } else {
+        Serial.print("\tDetector ");
+        Serial.print(x + 1);
+        Serial.println(" offline");
+        errors = true;
+      }
+    }
+
     disableMuxPort(x);
   }
-
   
+  Serial.println("\tSetting up detectors complete.");
 
-//  for (int x = 0 ; x < HISTORY_SIZE ; x++)
-//    history[x] = 150;
-//
-//  int calHistory[500];
-//  
-//  for(int i = 0; i < 500; i++) {
-//    distanceSensor.startRanging(); //Write configuration block of 135 bytes to setup a measurement
-//    calHistory[i] = distanceSensor.getDistance(); //Get the result of the measurement from the sensor
-//    distanceSensor.stopRanging();
-//  }
-//
-//    
-//
-//  for (int i = 0 ; i < 500 ; i++)
-//    cal += calHistory[i];
-//
-//  cal /= 500;
-//
-//  Serial.println(cal);
+  bootLight(false);
+
+  // Setup interrupt
+  pinMode(INTERRUPT_PIN, INPUT_PULLUP);
+  attachInterrupt(INTERRUPT_PIN, cancelLight, FALLING);
+  
+  // Finish booting
+  if(errors) {
+    Serial.println("Some errors occur while booting");
+    errorLight();   
+  } else {
+    Serial.println("Ready");
+    LEDS.setBrightness(50);
+  }
 }
 
-void loop(void)
-{
-
+void loop(void) {
   for (byte x = 0 ; x < NUMBER_OF_SENSORS ; x++) {
-    enableMuxPort(x); //Tell mux to connect to this port, and this port only
+    enableMuxPort(x);
 
-    //long startTime = millis();
-    distanceSensor.startRanging(); //Write configuration block of 135 bytes to setup a measurement
-    int distance = distanceSensor.getDistance(); //Get the result of the measurement from the sensor
+    distanceSensor.startRanging();
+    int distance = distanceSensor.getDistance(); 
     distanceSensor.stopRanging();
-    //long endTime = millis();
+    
     Serial.print("Sensor: ");
     Serial.print(x);
     Serial.print("\tDistance(mm): ");
     Serial.print(distance);
-  Serial.println();
-//    history[historySpot] = distance;
-//    if (++historySpot == HISTORY_SIZE) historySpot = 0;
-//  
-//    long avgDistance = 0;
-//    for (int x = 0 ; x < HISTORY_SIZE ; x++)
-//      avgDistance += history[x];
-//  
-//    avgDistance /= HISTORY_SIZE;
-//    Serial.print("\tavgDistance: ");
-//    Serial.print(avgDistance);
-//  
-//    Serial.print("\tHz: ");
-//    Serial.print(1000.0 / (float)(endTime - startTime), 2);
-//  
-//    if(avgDistance > (cal -20) && board) {
-//      board = false;
-//      Serial.print("\tBoard Not Detected");
-//      //Serial.println();
-//    } else if (avgDistance <= (cal -20) && !board) {
-//      board = true;
-//      Serial.print("\tBoard Detected");
-//      //Serial.println();
-//    }
+    //Serial.println();
 
+    if(clearReading > 2) {
+      history[x][historySpot[x]] = distance;
+    } else {
+      clearReading++;
+    }
+    if (++historySpot[x] == HISTORY_SIZE) historySpot[x] = 0;
+
+    long avgDistance = 0;
+    for (int i = 0 ; i < HISTORY_SIZE ; i++)
+      avgDistance += history[x][i];
+
+    avgDistance /= HISTORY_SIZE;
+    Serial.print("\tavgDistance: ");
+    Serial.print(avgDistance);
+
+    if(avgDistance > (calibration[x] - DIFF_FACTOR) && board) {
+      board = false;
+      Serial.print("\tBoard Not Detected");
+      //Serial.println();
+    } else if (avgDistance <= (calibration[x] - DIFF_FACTOR) && !board) {
+      board = true;
+      Serial.print("\tBoard Detected");
+      //Serial.println();
+   }
+
+Serial.println();
     disableMuxPort(x); //Tell mux to disconnect from this port
+
+    if(board) {
+      break;
+    } 
   }
 
-  delay(1); //Wait for next reading
-  
-  
+  if(board) {
+    unsigned long startTime, startTimeDiff;
 
-//  float distanceInches = avgDistance * 0.0393701;
-//  float distanceFeet = distanceInches / 12;
-//
-//  Serial.print("\tavgDistance(ft): ");
-//  Serial.print(distanceFeet, 2);
+    startTime = millis();
+    cancelled = false;
+    
+    while(!cancelled) {
+      startTimeDiff = millis() - startTime;
+    
+      if(startTimeDiff < 15000) {
+        updateLight(0,255,0,20);
+      } else if(startTimeDiff < 25000) {
+        updateLight(255,215,0,20);
+      } else {
+        updateLight(255,0,0,20);
+      }
+    }
+    // Turn light off
+    lightOff();
 
-//  int signalRate = distanceSensor.getSignalRate();
-//  Serial.print("\tSignal rate: ");
-//  Serial.print(signalRate);
-//
-//  byte rangeStatus = distanceSensor.getRangeStatus();
-//  Serial.print("\tRange Status: ");
-
-  //Make it human readable
-//  switch (rangeStatus)
-//  {
-//    case 0:
-//      Serial.print("Good");
-//      break;
-//    case 1:
-//      Serial.print("Signal fail");
-//      break;
-//    case 2:
-//      Serial.print("Sigma fail");
-//      break;
-//    case 7:
-//      Serial.print("Wrapped target fail");
-//      break;
-//    default:
-//      Serial.print("Unknown: ");
-//      Serial.print(rangeStatus);
-//      break;
-//  }
-
-  
-
-  
+    // Reset history
+    clearReading = 0;
+    for (byte x = 0 ; x < NUMBER_OF_SENSORS ; x++) {
+      for (int i = 0 ; i < HISTORY_SIZE ; i++) {
+        history[x][i] = calibration[x];
+      }
+    }
+    
+    // Wait
+    delay(10000);
+  } else {
+    delay(1);
+  }
 }
